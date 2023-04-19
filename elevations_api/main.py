5081708f5e9487192ebc8c2482fa2ece167c1b45
cortadocodes <cortado.codes@protonmail.com>
2023-04-19 14:40:14 +0100

    DOC: Add docstrings

--- elevations_api/main.py
+++ elevations_api/main.py
@@ -17,7 +17,7 @@ driver = GraphDatabase.driver(
 )
 
 POPULATION_WAIT_TIME = 240  # 4 minutes.
-recently_requested_for_population_cache = TTLCache(maxsize=1024, ttl=POPULATION_WAIT_TIME)
+recently_requested_for_database_population_cache = TTLCache(maxsize=1024, ttl=POPULATION_WAIT_TIME)
 
 
 ELEVATIONS_POPULATOR_PROJECT = "windeurope72-private"
@@ -33,7 +33,7 @@ def get_elevations(request):
     requested_cells = set(request.get_json()["h3_cells"])
     logger.info("Received request for elevations at the H3 cells: %r.", requested_cells)
 
-    available_cells_and_elevations = _get_elevations_from_database(requested_cells)
+    available_cells_and_elevations = _get_available_elevations_from_database(requested_cells)
     unavailable_cells = requested_cells - available_cells_and_elevations.keys()
     cells_to_populate = _extract_cells_to_populate(unavailable_cells)
 
@@ -45,7 +45,12 @@ def get_elevations(request):
     return jsonify({"elevations": available_cells_and_elevations, "later": list(unavailable_cells)})
 
 
-def _get_elevations_from_database(cells):
+def _get_available_elevations_from_database(cells):
+    """Get the elevations of the given cells from the database if they're available.
+
+    :param iter(int) cells: the indexes of the cells to attempt getting the elevations for
+    :return dict(int, float): a mapping of cell index to elevation for cells that have elevations in the database. The elevation is measured in meters.
+    """
     logger.info("Checking database for elevation data...")
     indexes = " or ".join(f"c.index = {cell}" for cell in cells)
 
@@ -63,17 +68,36 @@ def _get_elevations_from_database(cells):
 
 
 def _extract_cells_to_populate(unavailable_cells):
-    cells_to_await = unavailable_cells & recently_requested_for_population_cache.keys()
+    """Extract the cells to request database population for from the set of cells that don't have elevations in the
+    database. This filters out the cells that have recently had database population requested for them to avoid
+    requesting population for any cells twice.
+
+    :param set(int) unavailable_cells: the set of cell indexes that aren't in the database
+    :return set(int): the subset of the unavailable cells that haven't recently had database population requested for them
+    """
+    cells_to_await = unavailable_cells & recently_requested_for_database_population_cache.keys()
     logger.info("Still waiting for %d cells to be populated in database.", len(cells_to_await))
     cells_to_populate = unavailable_cells - cells_to_await
     return cells_to_populate
 
 
 def _add_cells_to_ttl_cache(cells):
-    recently_requested_for_population_cache.update((cell, None) for cell in cells)
+    """Add the cells to the cache of cell indexes that have recently been requested for database population. These cells
+    will remain in the cache until the population wait time has been exceeded and will not be re-requested for database
+    population in that time.
+
+    :param iter(int) cells: the cells to add to the cache for the population wait time
+    :return None:
+    """
+    recently_requested_for_database_population_cache.update((cell, None) for cell in cells)
 
 
 def _populate_database(cells):
+    """Request that the given cells elevations are found and added to the database.
+
+    :param iter(int) cells: the cells to request database population for
+    :return None:
+    """
     logger.info("Requesting database population for %d cells.", len(cells))
     service = Service(backend=GCPPubSubBackend(project_name=ELEVATIONS_POPULATOR_PROJECT))
     service.ask(service_id=ELEVATIONS_POPULATOR_SERVICE_SRUID, input_values={"h3_cells": list(cells)})
