de62583d8140af7bb0d5ea7e7220190b42ee6607
cortadocodes <cortado.codes@protonmail.com>
2023-04-19 15:50:58 +0100

    TST: Test when no cells are available

--- tests/test_main.py
+++ tests/test_main.py
@@ -23,16 +23,36 @@ class TestMain(unittest.TestCase):
         self.assertEqual(response, (f"Only {cell_limit} cells can be sent per request.", 400))
 
     def test_all_cells_available(self):
-        """Test that, when the input cells all already have elevations for them in the database, the response just
-        contains their elevations.
+        """Test that, when all the input cells already have elevations in the database, database population is not
+        requested and the response just contains their elevations.
         """
         input = {"h3_cells": [1, 2]}
         request = Mock(method="POST", get_json=Mock(return_value=input), args=input)
         mock_elevations = {1: 32.1, 2: 59}
 
         with patch("elevations_api.main._get_available_elevations_from_database", return_value=mock_elevations):
-            # Mock `jsonify` to avoid needing Flask app context or test app.
-            with patch("elevations_api.main.jsonify") as mock_jsonify:
-                get_or_request_elevations(request)
+            with patch("elevations_api.main._populate_database") as mock_populate_database:
+                # Mock `jsonify` to avoid needing Flask app context or test app.
+                with patch("elevations_api.main.jsonify") as mock_jsonify:
+                    get_or_request_elevations(request)
 
         mock_jsonify.assert_called_with({"elevations": mock_elevations})
+        mock_populate_database.assert_not_called()
+
+    def test_all_cells_unavailable(self):
+        """Test that, when all the input cells don't have elevations in the database, database population is requested
+        and the response contains an empty elevations list and a `later` list of the input cells.
+        """
+        input = {"h3_cells": [1, 2]}
+        request = Mock(method="POST", get_json=Mock(return_value=input), args=input)
+
+        with patch("elevations_api.main._get_available_elevations_from_database", return_value={}):
+            with patch("elevations_api.main._populate_database") as mock_populate_database:
+                # Mock `jsonify` to avoid needing Flask app context or test app.
+                with patch("elevations_api.main.jsonify") as mock_jsonify:
+                    get_or_request_elevations(request)
+
+        response = mock_jsonify.call_args.args[0]
+        self.assertEqual(response["elevations"], {})
+        self.assertEqual(response["later"], [1, 2])
+        mock_populate_database.assert_called()
