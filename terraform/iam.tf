7a6a99b888b2078f9aee0f172b8f521e644ce1e9
cortadocodes <cortado.codes@protonmail.com>
2023-04-19 12:12:54 +0100

    OPS: Give API service account PubSub permissions

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -53,6 +53,7 @@ resource "google_project_iam_binding" "pubsub_editor" {
   role    = "roles/pubsub.editor"
   members = [
     "serviceAccount:${google_service_account.elevations_populator_service_account.email}",
+    "serviceAccount:${google_service_account.elevations_api_service_account.email}",
     "serviceAccount:${google_service_account.github_actions_service_account.email}",
     "serviceAccount:${google_service_account.dev_cortadocodes_service_account.email}"
   ]
