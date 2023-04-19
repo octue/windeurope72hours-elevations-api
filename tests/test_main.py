8968890d04ca72ddd8d588b2a333c091eb4a5f52
cortadocodes <cortado.codes@protonmail.com>
2023-04-19 15:45:10 +0100

    TST: Test when all elevations are available

--- tests/test_main.py
+++ tests/test_main.py
@@ -13,11 +13,26 @@ class TestMain(unittest.TestCase):
 
     def test_error_response_raised_if_cell_limit_exceeded(self):
         """Test that an error response is returned if the number of cells in the request exceeds the cell limit."""
-        data = {"h3_cells": [1, 2]}
-        request = Mock(method="POST", get_json=Mock(return_value=data), args=data)
+        input = {"h3_cells": [1, 2]}
+        request = Mock(method="POST", get_json=Mock(return_value=input), args=input)
         cell_limit = 1
 
         with patch("elevations_api.main.CELL_LIMIT", cell_limit):
             response = get_or_request_elevations(request)
 
         self.assertEqual(response, (f"Only {cell_limit} cells can be sent per request.", 400))
+
+    def test_all_cells_available(self):
+        """Test that, when the input cells all already have elevations for them in the database, the response just
+        contains their elevations.
+        """
+        input = {"h3_cells": [1, 2]}
+        request = Mock(method="POST", get_json=Mock(return_value=input), args=input)
+        mock_elevations = {1: 32.1, 2: 59}
+
+        with patch("elevations_api.main._get_available_elevations_from_database", return_value=mock_elevations):
+            # Mock `jsonify` to avoid needing Flask app context or test app.
+            with patch("elevations_api.main.jsonify") as mock_jsonify:
+                get_or_request_elevations(request)
+
+        mock_jsonify.assert_called_with({"elevations": mock_elevations})
